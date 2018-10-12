3904d9940833d45f58889b4e32f237f5c948644b
Fedward <31490192+Fedward@users.noreply.github.com>
2018-10-12 17:06:16 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,56 +1,68 @@
 "use strict";
 
 let cells = [],
-	startButton;
+	startButton,
+	testObj = {};
 
 window.onload = function () {
 	startButton = document.getElementById("startButton");
-	// startButton.onclick = startRotate;	
+	startButton.addEventListener("click", startRotate);
 	cells = document.querySelectorAll("#container p");
 }
 
 function startRotate(){	
-	startButton.removeAttribute("onclick");
+	startButton.removeEventListener("click", startRotate);
 	startButton.innerHTML = "In progress";
 	console.log("Starting rotation!...");
 	let i = 0;
 	let interval = setInterval(rotate, 250);
+	const handler = rotateEnd(i);
 
 	function rotate(){
 		console.log("Cell №", i + 1, "rotate START");
 
-		setTimeout((function(index){
+/*		setTimeout((function(index){
 			return function(){
-				console.log("Cell №", index + 1, "rotate END");
-				if ( index + 1 === cells.length ) {
+				console.log("Cell №", index + 1, "rotate END"); // старая реализация с использованием setTimeout без 
+				if ( index + 1 === cells.length ) {				// без EventListener - transitionend
 					console.log("Finish!");
 					alert("Success!");	
 					reset();				
 				}
 			}			
-		})(i), 1000);
+		})(i), 1000);*/
 
-		// let currentAttribute = cells[i].getAttribute("class");	
-		// cells[i].setAttribute("class", currentAttribute + " rotate");
-		cells[i].classList.add("rotate");
+		function rotateEnd(index) {
+			return function (){
+				console.log("Cell №", index + 1, "rotate END");
+				if ( index + 1 === cells.length) {
+					console.log("Finish!");
+					alert("Success!");	
+					reset();					
+				}
+			}
+		}
+
+		function reset(){
+			startButton.innerHTML = "Start";
+			startButton.addEventListener("click", startRotate);
+			for (let i = 0; i < cells.length; i++) {
+				cells[i].removeEventListener("transitionend", handler);
+				cells[i].classList.toggle("rotate");			
+		/*		if (cells[i].classList.length === 1 && cells[i].classList[0] === "rotate") {
+					cells[i].removeAttribute("class");	// способ, если не нужно, чтобы оставался пустой атрибут class у ячеек
+				} else {
+				cells[i].classList.toggle("rotate");
+				};*/	
+			}
+		}		
+
+		cells[i].addEventListener("transitionend", rotateEnd(i));
+		cells[i].classList.toggle("rotate");
 		i++;
 		if ( i === cells.length) {
 			clearInterval(interval);
 		}
-	}
-}
-
-function reset(){
-	startButton.innerHTML = "Start";
-	// startButton.onclick = startRotate;
-	startButton.setAttribute("onclick", "startRotate()");	
-	for (let i = 0; i < cells.length; i++) {
-		// cells[i].setAttribute("class", "cell");
-		if (cells[i].classList == "rotate") {
-			cells[i].removeAttribute("class");
-		} else {
-		cells[i].classList.remove("rotate");
-		};
 	}
 }
 
@@ -60,3 +72,59 @@ function reset(){
         	-------------------------------
         });
     } catch (e) { } */
+
+
+
+// рабочий варик на эвентах
+
+/*    "use strict";
+
+let cells = [],
+	startButton;
+
+window.onload = function () {
+	startButton = document.getElementById("startButton");
+	startButton.addEventListener("click", startRotate);
+	cells = document.querySelectorAll("#container p");
+}
+
+function startRotate(){	
+	startButton.removeEventListener("click", startRotate);
+	startButton.innerHTML = "In progress";
+	console.log("Starting rotation!...");
+	let i = 0;
+	let interval = setInterval(rotate, 250);
+
+	function rotate(){
+		console.log("Cell №", i + 1, "rotate START");
+    const handler = rotateEnd(i);
+
+		function rotateEnd(index) {
+			return function (){
+				console.log("Cell №", index + 1, "rotate END");
+        cells[index].removeEventListener("transitionend", handler);
+        
+				if ( index + 1 === cells.length) {
+					console.log("Finish!");
+					alert("Success!");	
+					reset();					
+				}
+			}
+		}
+
+		function reset(){
+			startButton.innerHTML = "Start";
+			startButton.addEventListener("click", startRotate);
+			for (let i = 0; i < cells.length; i++) {
+        cells[i].classList.toggle("rotate");	
+			}
+		}		
+
+		cells[i].addEventListener("transitionend", handler);
+		cells[i].classList.toggle("rotate");
+		i++;
+		if ( i === cells.length) {
+			clearInterval(interval);
+		}
+	}
+}*/
